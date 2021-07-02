5ff58f638b8ef803817c9f0f074ef77ab4fc3acb
Yikai Lu <yikailu@buffalo.edu>
2021-07-01 20:43:36 -0400

    Add files via upload

---
+++ Postprocessing.py
@@ -0,0 +1,78 @@
+
+#######################################################################################################################
+# YOU MUST FILL OUT YOUR SECONDARY OPTIMIZATION METRIC (either accuracy or cost)!
+# The metric chosen must be the same for all 5 methods.
+#
+# Chosen Secondary Optimization Metric: #
+#######################################################################################################################
+""" Determines the thresholds such that each group has equal predictive positive rates within 
+    a tolerance value epsilon. For the Naive Bayes Classifier and SVM you should be able to find
+    a nontrivial solution with epsilon=0.02. 
+    Chooses the best solution of those that satisfy this constraint based on chosen 
+    secondary optimization criteria.
+"""
+def enforce_demographic_parity(categorical_results, epsilon):
+    demographic_parity_data = {}
+    thresholds = {}
+
+    # Must complete this function!
+    #return demographic_parity_data, thresholds
+
+    return None, None
+
+#######################################################################################################################
+""" Determine thresholds such that all groups have equal TPR within some tolerance value epsilon, 
+    and chooses best solution according to chosen secondary optimization criteria. For the Naive 
+    Bayes Classifier and SVM you should be able to find a non-trivial solution with epsilon=0.01
+"""
+def enforce_equal_opportunity(categorical_results, epsilon):
+
+    thresholds = {}
+    equal_opportunity_data = {}
+
+    # Must complete this function!
+    #return equal_opportunity_data, thresholds
+
+    return None, None
+
+#######################################################################################################################
+
+"""Determines which thresholds to use to achieve the maximum profit or maximum accuracy with the given data
+"""
+
+def enforce_maximum_profit(categorical_results):
+    mp_data = {}
+    thresholds = {}
+
+    # Must complete this function!
+    #return mp_data, thresholds
+
+    return None, None
+
+#######################################################################################################################
+""" Determine thresholds such that all groups have the same PPV, and return the best solution
+    according to chosen secondary optimization criteria
+"""
+
+def enforce_predictive_parity(categorical_results, epsilon):
+    predictive_parity_data = {}
+    thresholds = {}
+
+    # Must complete this function!
+    #return predictive_parity_data, thresholds
+
+    return None, None
+
+    ###################################################################################################################
+""" Apply a single threshold to all groups, and return the best solution according to 
+    chosen secondary optimization criteria
+"""
+
+def enforce_single_threshold(categorical_results):
+    single_threshold_data = {}
+    thresholds = {}
+
+    # Must complete this function!
+    #return single_threshold_data, thresholds
+
+    return None, None
